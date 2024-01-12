dcaff9367e4fad21aa23f94cb1c3af2b7959c478
PaolaSL12 <paola.sanchezl12@gmail.com>
2024-01-12 16:44:33 +0100

    finish exercices

--- Ejercicio1/index.js
+++ Ejercicio1/index.js
@@ -1,7 +1,15 @@
 // Crea una arrow function que tenga dos parametros a y b y 
-// que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre 
+// que por defecto el valor de a = 10 y de b = 5. Haz que la fuclrnción muestre 
 // por consola la suma de los dos parametros.
 
+const arrow = (a = 10, b = 5) => {
+ return a + b;
+}
+
+
 // 1.1 Ejecuta esta función sin pasar ningún parametro
+console.log(arrow());
 // 1.2 Ejecuta esta función pasando un solo parametro
+console.log(arrow(5));
 // 1.3 Ejecuta esta función pasando dos parametros
+console.log(arrow(10, 10));
---
+++ Ejercicio14/index.js
@@ -0,0 +1,12 @@
+// Crea una función llamada rollDice() que reciba como parametro el numero de caras 
+// que queramos que tenga el dado que deberá simular el codigo dentro de la función. 
+// Como hemos dicho, que la función use el parametro para simular una tirada de dado 
+// y retornar el resultado. Si no se te ocurre como hacer un numero aleatorio no te 
+// preocupes! Busca información sobre la función de javascript Math.random()
+
+const rollDice = (diceSides) => {
+ return Math.floor(Math.random() * diceSides) + 1;
+
+}
+rollDice()
+console.log(rollDice(6));
